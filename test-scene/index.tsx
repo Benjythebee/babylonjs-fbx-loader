757dde7a68db6d680342ef3d459072c24d9888fa
Benjythebee <Benjy.larcher@aol.com>
2022-09-20 13:16:13 +1200

    scale down the model and add animations

--- test-scene/index.tsx
+++ test-scene/index.tsx
@@ -9,8 +9,27 @@ if (SceneLoader) {
   SceneLoader.RegisterPlugin(new FBXLoader())
 }
 
+scene.onPointerDown = (evt, pickResult) => {
+  const mesh = pickResult.pickedMesh
+  if (mesh) {
+    console.log(mesh)
+    ;(window as any).mesh = mesh
+  }
+}
+
 setTimeout(async () => {
-  let mesh = await SceneLoader.ImportMeshAsync(null, 'https://dl.dropboxusercontent.com/s/', '8s3d16fs5h4scmk/wolf_w_texture.fbx?dl=0', scene!, (e) => console.log(e))
-  console.log(mesh)
-  ;(scene.activeCamera! as ArcRotateCamera).setTarget(new Vector3(...mesh.meshes[0].position.asArray()))
+  let result = await SceneLoader.ImportMeshAsync(null, 'https://dl.dropboxusercontent.com/s/', '8s3d16fs5h4scmk/wolf_w_texture.fbx?dl=0', scene!)
+  console.log(result)
+  result.meshes.forEach((mesh) => {
+    mesh.isPickable = true
+    mesh.scaling.scaleInPlace(0.1)
+  })
+  ;(scene.activeCamera! as ArcRotateCamera).setPosition(new Vector3(32, 39, 47))
+  ;(scene.activeCamera! as ArcRotateCamera).setTarget(new Vector3(...result.meshes[0].position.asArray()))
+  //Get the Samba animation Group
+  const idle = scene.getAnimationGroupByName('AnimStack::Wolf_Skeleton|Wolf_Skeleton|Wolf_Skeleton|Wolf_Idle_|Wolf_Skeleton|Wolf_Idle_')
+  if (idle) {
+    //Play the Samba animation
+    idle.start(true, 1.0, idle.from, idle.to, false)
+  }
 }, 1000)
